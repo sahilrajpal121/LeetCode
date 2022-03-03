16afd19cfb86ce2b7164d2726dd742784c39b775
Sahil Rajpal <40625432+sahilrajpal121@users.noreply.github.com>
2022-03-03 16:04:32 +0530

    Time: 40 ms (85.03%), Space: 14.1 MB (75.02%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -4,23 +4,18 @@ class Solution:
         if len(nums) < 3:
             return 0
         
-        subs = []
-        for i in range(1, len(nums)-1):
-            val = (nums[i] - nums[i-1]) == (nums[i+1] - nums[i])
-            subs.append(val)
-        
-        curr = 0
+        prev_diff = nums[1] - nums[0]
         s = 0
-        for sub in subs:
-            if sub:
+        curr = 0
+        for i in range(2, len(nums)):
+            diff = nums[i] - nums[i-1]
+            
+            if diff == prev_diff:
                 curr += 1
             else:
-                # if curr > 1:
-                #     s += curr + 1
-                # elif curr == 1:
-                #     s += 1
-                s += curr * (curr + 1) // 2
+                prev_diff = diff
                 curr = 0
-        if curr:
-            s += curr * (curr + 1) // 2
-        return s     
+            
+            s += curr
+            
+        return s
